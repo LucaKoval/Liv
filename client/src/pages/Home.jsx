d959b02f7541c2d61a4489aab38f54994dccfa67
Luca Koval <lucakoval@me.com>
2020-02-01 21:43:57 -0500

    Fixing more className

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -12,15 +12,15 @@ class Home extends Component {
     render() {
         return (
             <div>
-                <div class = "title-main">
-                    <div class = "title"><b>liv</b><br/></div>
-                    <div class = "subtitle">Swedish for life, <em>liv</em> is a <b>wearable medical assistant</b> on your wrist that 
+                <div className = "title-main">
+                    <div className = "title"><b>liv</b><br/></div>
+                    <div className = "subtitle">Swedish for life, <em>liv</em> is a <b>wearable medical assistant</b> on your wrist that 
                         automates the process of seeking emergency care.</div>
                 </div>
                 
-                <div class = "about">
-                    <div class = "description-name"><b>How does liv work?</b><br/></div>
-                    <div class = "description"><b>Liv is designed for anyone who is at risk of a medical emergency</b>, whether they are
+                <div className = "about">
+                    <div className = "description-name"><b>How does liv work?</b><br/></div>
+                    <div className = "description"><b>Liv is designed for anyone who is at risk of a medical emergency</b>, whether they are
                         an elderly person diagnosed with Parkinson's, an adult with diabetes, or a young child with a serious allergy.
                         Liv stores important personal and medical information such as emergency contacts, medical condition, and current medications in the wearer's profile. 
                         Each wearer is identified by a unique user ID.
